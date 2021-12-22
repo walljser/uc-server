49830b0c4320c6bb632a4912f7d20f0d9cf288dd
yushijie <walljs@163.com>
2021-12-22 16:10:38 +0800

    typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { DATABASE_USERNAME, DATABASE_PASSWORD } from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: DATABASE_USERNAME,
+      password: DATABASE_PASSWORD,
+      database: 'user_center',
+      entities: [],
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/database.config.ts
@@ -0,0 +1,2 @@
+export const DATABASE_USERNAME = 'root';
+export const DATABASE_PASSWORD = 'root';
